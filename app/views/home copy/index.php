4872cac3979c9d106333b400018d33d5d0d03725
balakrish0008@gmail.com <balakrish0008@gmail.com>
2022-06-21 09:07:38 +0530

    db name change

--- app/views/home copy/index.php
+++
@@ -1,6 +0,0 @@
-<?php require APPROOT . '/views/home/_inc/header.php'; ?>
-<?php require APPROOT . '/views/home/_inc/nav.php'; ?>
-<?php require APPROOT . '/views/home/_inc/footer.php'; ?>
-
-
-    
